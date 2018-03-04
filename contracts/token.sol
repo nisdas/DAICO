2d40684f0ddd423e7b9332eafdb4b457bdf75603
nisdas <nish1993@hotmail.com>
2018-03-04 17:53:25 +0800

    Adding ERC20 interface, SafeMath Lib

--- contracts/token.sol
+++ contracts/token.sol
@@ -1,5 +1,65 @@
 pragma solidity ^0.4.18;
 
-interface DAICO {
+import "./ERC20.sol";
+import "./SafeMath.sol";
 
+contract token is ERC20 {
+
+    using SafeMath for uint256;
+
+    mapping(address => uint256) balances;
+    mapping (address => mapping (address => uint256)) internal allowed;
+    string public name;
+    string public symbol;
+    uint8 public decimals;
+
+
+    function token( uint256 _initialAmount, string _tokenName, string _tokenSymbol, uint8 _decimals) public {
+        balances[msg.sender] = _initialAmount;               
+        totalSupply = _initialAmount;                        
+        name = _tokenName;                                                               
+        symbol = _tokenSymbol; 
+        decimals = _decimals;                              
+    }
+
+
+    function balanceOf(address who) public view returns (uint256) {
+        return balances[who];
+    }
+
+      function transfer(address _to, uint256 _value) public returns (bool) {
+    require(_to != address(0));
+    require(_value <= balances[msg.sender]);
+
+    balances[msg.sender] = balances[msg.sender].sub(_value);
+    balances[_to] = balances[_to].add(_value);
+    Transfer(msg.sender, _to, _value);
+    return true;
+  }
+
+   function transferFrom(address _from, address _to, uint256 _value) public returns (bool) {
+    require(_to != address(0));
+    require(_value <= balances[_from]);
+    require(_value <= allowed[_from][msg.sender]);
+
+    balances[_from] = balances[_from].sub(_value);
+    balances[_to] = balances[_to].add(_value);
+    allowed[_from][msg.sender] = allowed[_from][msg.sender].sub(_value);
+    Transfer(_from, _to, _value);
+    return true;
+  }
+
+  function approve(address _spender, uint256 _value) public returns (bool) {
+    allowed[msg.sender][_spender] = _value;
+    Approval(msg.sender, _spender, _value);
+    return true;
+  }
+
+  function allowance(address _owner, address _spender) public view returns (uint256) {
+    return allowed[_owner][_spender];
+  } 
+
+
+
+    
 }
