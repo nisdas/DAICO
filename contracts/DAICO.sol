14eb0e2f820ad48f9712d692a3d4568e0a34264b
nisdas <nish1993@hotmail.com>
2018-03-04 19:47:39 +0800

    Adding Tap Variable

--- contracts/DAICO.sol
+++ contracts/DAICO.sol
@@ -10,6 +10,15 @@ contract DAICO is Crowdsale {
 
   uint256 public openingTime;
   uint256 public closingTime;
+  uint256 public TapSet;
+
+  /* The length the funds are held in the contract till the full amount is 
+     ditributed to the team(in seconds)
+  */
+  uint256 public tapPeriod;
+
+  //Tap Rate at which developers can withdraw funds(wei/sec)
+  uint256 public tap;
 
 
   modifier onlyWhileOpen {
@@ -17,18 +26,36 @@ contract DAICO is Crowdsale {
     _;
   }
 
-
-  function DAICO(uint256 _openingTime, uint256 _closingTime) public {
-    require(_openingTime >= now);
-    require(_closingTime >= _openingTime);
-
-    openingTime = _openingTime;
-    closingTime = _closingTime;
+  modifier setTap {
+   require(TapSet == 0);
+   _;
+   TapSet += 1;
+  }
+  
+  modifier onlyWhileClosed {
+      require(now >= closingTime);
+      _;   
   }
 
 
-  function hasClosed() public view returns (bool) {
+  function DAICO(uint256 _openingTime, uint256 _closingTime, uint256 _tapPeriod) public {
+    require(_openingTime >= now);
+    require(_closingTime >= _openingTime);
+    require(_tapPeriod >= 2);
+
+    tapPeriod = _tapPeriod * 365 * 24 * 60 * 60;
+    openingTime = _openingTime;
+    closingTime = _closingTime;
+
+  }
+
+
+  function _hasClosed() public view returns (bool) {
     return now > closingTime;
+  }
+
+  function _setTap() internal onlyWhileClosed {
+      tap = this.balance / tapPeriod ;
   }
 
   function _preValidatePurchase(address _beneficiary, uint256 _weiAmount) internal onlyWhileOpen {
